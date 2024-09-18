0df4d44fafc6fbd17396b2f44e3b5566dafddeab
DT <you@example.com>
2024-09-18 23:35:37 +0200

    emails mit 2 rezepten gehen ein

--- send_mail.py
+++ send_mail.py
@@ -1,4 +1,5 @@
 from mailjet_rest import Client
+import random
 import json
 
 # Lade die Konfigurationsdaten aus der config.json
@@ -11,8 +12,27 @@ api_secret = config['api_secret']
 to_email = config['sendto']
 from_email = config['username']
 
-subject = "Dein Rezept"
-email_body = "Hier ist dein zufällig ausgewähltes Rezept!"
+# Lade die Rezepte
+with open('recipes.json', 'r') as f:
+    recipes = json.load(f)
+
+# Funktion zur Auswahl von zwei zufälligen Rezepten
+def get_random_recipes(recipes, num=2):
+    return random.sample(recipes, min(num, len(recipes)))
+
+# Wähle zwei zufällige Rezepte aus
+random_recipes = get_random_recipes(recipes)
+
+# Erstelle den E-Mail-Text mit den Rezepten
+recipe_texts = []
+for recipe in random_recipes:
+    recipe_texts.append(f"{recipe['title']}: {recipe['link']}")
+
+# E-Mail-Inhalt
+email_body = "Hier sind deine zufällig ausgewählten Rezepte:\n\n" + "\n".join(recipe_texts)
+
+# Betreff (Subject) dynamisch mit den Rezeptnamen
+subject = f"Rezepte: {random_recipes[0]['title']} & {random_recipes[1]['title']}"
 
 def send_email(subject, body, to_email, from_email):
     mailjet = Client(auth=(api_key, api_secret), version='v3.1')
